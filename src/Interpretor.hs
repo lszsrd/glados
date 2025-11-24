3ffca6c507c2d1b223129c90476167c8d58bc40b
Anexoms <maxence.pierre@epitech.eu>
2025-11-24 22:00:05 +0100

    < :rocket: >[Entrypoint/main] begin main architecture

---
+++ src/Interpretor.hs
@@ -0,0 +1,25 @@
+{- 
+-- EPITECH PROJECT, 2025
+-- GENERIC LANGUAGE AND DATA OPERAND SYNTAX
+-- File description:
+-- src/Parser.hs
+-}
+
+module Interpretor (interpretResult) where
+
+import Parser (Ast(..))
+import Control.Exception (try, SomeException, evaluate)
+import Error
+
+-- Ram through the AST and interpret it  
+interpret :: Parser.Ast -> String
+interpret (TODO _) = throwErr $ Error.ErrorT { location = 0, message = "No input" } 
+interpret _ = "passed"
+
+interpretResult :: Ast -> IO String
+interpretResult ast = do
+    evalParser <- try (evaluate (interpret ast))
+        :: IO (Either SomeException String)
+    case evalParser of
+        Left err -> printError (show err)
+        Right str -> return str
