d424d237995f753565d1706e7650372657f5aa22
h <hugo.duda@protonmail.com>
2025-11-26 21:30:14 +0100

    < :building_construction: >[dev/AST] Update AST

--- src/Parser.hs
+++ src/Parser.hs
@@ -11,25 +11,23 @@ import Lexer (Token(..))
 import Control.Exception (try, SomeException, evaluate)
 import Error
 
-type Identifier             = String
+type Identifier    = String
 
-
-data Ast = Define Identifier Ast
-    | FunctionCall Identifier [Ast]
-    | IF Ast Ast Ast -- if expr then expr2 else expr3
-    | Lambda [Ast] Ast
-    | Cond [Ast] -- cond has a list of cond and execute until one is true
-    | Variable Identifier
-    | Constant Integer
-    | Boolean Bool
+data Ast = ExprDefine  Identifier Ast
+    |      ExprLambda  Identifier [Ast]   
+    |      ExprIf      Ast Ast Ast
+    |      ExprVar     Identifier
+    |      ExprBool    Bool
+    |      ExprInteger Integer
+    |      ExprApp     Identifier [Ast]
     deriving Show
 
--- Parse and create the AST with the given TokenList 
+-- Parse and create the AST with the given TokenList
 parsor :: [Lexer.Token] -> Ast
-parsor [] = throwErr $ Error.ErrorT { location = 0, message = "No input" } 
-parsor _ = MT "passed"
+parsor [] = throwErr $ Error.ErrorT { location = 0, message = "No input" }
+parsor _ = ExprVar "passed"
 
-getAST :: [Token] -> IO Ast
+getAST :: [Lexer.Token] -> IO Ast
 getAST tkList = do
     evalParser <- try (evaluate (parsor tkList))
         :: IO (Either SomeException Ast)
