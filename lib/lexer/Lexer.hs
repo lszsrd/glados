ab6f99aa3b45273a8a1b18bb091216918bbe069d
lszsrd <laszlo.serdet@epitech.eu>
2025-11-27 14:32:35 +0100

    < :construction_worker: >[dev/tests] Fixing tests not running on epitest

--- lib/lexer/Lexer.hs
+++ lib/lexer/Lexer.hs
@@ -2,7 +2,7 @@
 -- EPITECH PROJECT, 2025
 -- Generic Language And Data Operand Syntax
 -- File description:
--- src/lexer/Lexer.hs
+-- lib/Lexer/Lexer.hs
 -}
 
 -------------------------------------------------------------------------------
