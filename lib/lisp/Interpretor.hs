f37dba130344c034f233938bddb2c2ecc498c7fc
Anexoms <maxence.pierre@epitech.eu>
2025-11-28 15:11:27 +0100

    < :construction: >[Interpretor/dataStruct] create temporary data structure for interpretors to work

--- lib/lisp/Interpretor.hs
+++ lib/lisp/Interpretor.hs
@@ -7,20 +7,35 @@
 
 module Interpretor (interpretResult) where
 
-import Parser (Ast(..))
+-- import Parser (Ast(..))
 import Control.Exception (try, SomeException, evaluate)
 import Error
 
+type Identifier     = String
+type Operator       = Identifier
+type Args           = [Identifier]
+data FunCall        = FunCall Operator Args Ast deriving Show
+
+data Ast = Define   Identifier Ast
+    |      Lambda   Identifier Args Ast
+    |      If       FunCall Ast Ast
+    |      Var      Identifier
+    |      Bool     Bool
+    |      Integer  Integer
+    |      Call     FunCall
+    deriving Show
+
 -- Ram through the AST and interpret it  
-interpret :: Parser.Ast -> String
+interpret :: Ast -> Ast
 {- interpret (TODO _) = throwErr $ Error.ErrorT
     { location = 0, message = "No input" }  -}
-interpret _ = "passed"
+interpret (Var x) = Var x
+interpret _ = Var "Nothing"
 
-interpretResult :: Ast -> IO String
+interpretResult :: Ast -> IO Ast
 interpretResult ast = do
     evalParser <- try (evaluate (interpret ast))
-        :: IO (Either SomeException String)
+        :: IO (Either SomeException Ast)
     case evalParser of
         Left err -> printError (show err)
         Right str -> return str
