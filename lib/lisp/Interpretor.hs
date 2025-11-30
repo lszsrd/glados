d0490b3e1a2768e575a8d24992bdedd0ffa1c0b7
nate <nathan.flachat@epitech.eu>
2025-11-30 21:04:35 +0100

    < 🚑 >[Interpretor/Fix] change function builtin return

--- lib/lisp/Interpretor.hs
+++ lib/lisp/Interpretor.hs
@@ -56,7 +56,7 @@ interpret (call@(Call f args): ast) env = if searchInEnv env call
 
 -- BUILTINS
 
-applyBuiltin :: String -> [Integer] -> (Either Integer Bool)
+applyBuiltin :: String -> [Integer] -> Maybe (Either Integer Bool)
 
 applyBuiltin _ _ = Nothing
 
