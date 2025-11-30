cd8ef0a9a005f227ea53fcf7e0ad940ac1e41994
nate <nathan.flachat@epitech.eu>
2025-11-30 17:03:57 +0100

    < 🚧 >[Interpretor/builtin] create basic builtin (+-/*><==), no error handling

--- lib/lisp/Interpretor.hs
+++ lib/lisp/Interpretor.hs
@@ -22,10 +22,8 @@ data Ast = Define   Identifier Args Ast
     |      Call     Identifier Args 
     deriving Show
 
-type Value = String
-
-data Env = Variable String Value
-    |      Function FunCall
+data Env = Variable String
+    | Function FunCall
     deriving Show
 
 searchInEnv :: [Env] -> Ast -> Bool
@@ -35,26 +33,15 @@ searchInEnv (Function(FunCall i1 args1 ast1):xs) fCall@(Call i2 args2) =
     -- TODO Need to verify types of args
 searchInEnv (_:xs) f = searchInEnv xs f
 
-eval :: Ast -> Maybe Ast
-eval _ = Just $ Bool True
-
-evalFunc :: FunCall -> Maybe Bool
-evalFunc _ = Just False
 
 -- Ram through the AST and interpret it  
 interpret :: [Ast] -> [Env] -> Maybe Ast
 interpret [] _ = Nothing
-
 interpret (Define i ag bd: ast) env =
     interpret ast (env ++ [Function (FunCall i ag bd)])
-
-interpret (If cond th el: ast) env = do
-    condEval <- evalFunc cond
-    if condEval then eval th else eval el
-
 interpret (call@(Call f args): ast) env = if searchInEnv env call
     then Just $ Var ("Call to " ++ show f ++ " Sucess")
-    else Nothing
+    else Just $ Var ("Call to " ++ show f ++ " Failed")
 
 -- interpretResult :: [Ast] -> IO Ast
 -- interpretResult ast = do
@@ -64,3 +51,41 @@ interpret (call@(Call f args): ast) env = if searchInEnv env call
 --         Left err -> printError (show err)
 --         Right str -> return str
 -- [(Define "foo" ["a", "b"] (Call "+" ["a", "b"]))]
+
+
+
+-- BUILTINS
+
+applyBuiltin :: String -> [Integer] -> Maybe Integer
+
+applyBuiltin _ _ = Nothing
+
+applyBuiltin "+" n = Just $ sum n
+
+applyBuiltin "-" n = case n of
+    []  -> Nothing
+    [x] -> Just (-x)
+    (x:xs) -> Just $ x - sum xs
+
+applyBuiltin "*" n = Just $ product n
+
+applyBuiltin "div" n = case n of
+    [a,b] -> if b == 0 then Nothing else Just (a `div` b)
+    _ -> Nothing
+
+
+applyBuiltin "mod" n = case n of
+    [a,b] -> if b == 0 then Nothing else Just $ Left (a `mod` b)
+    _ -> Nothing
+
+applyBuiltin "<" n = case n of
+    [a,b] -> Just $ Right (a < b)
+    _ -> Nothing
+
+applyBuiltin ">" n = case n of
+    [a,b] -> Just $ Right (a > b)
+    _ -> Nothing
+
+applyBuiltin "eq?" n = case n of
+    [a,b] -> Just $ Right (a == b)
+    _ -> Nothing
