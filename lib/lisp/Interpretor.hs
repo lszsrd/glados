564b9113710b17a03a303b47cc21bba3e5c85907
nate <nathan.flachat@epitech.eu>
2025-12-02 22:04:33 +0100

    < 🏗️ >[Interpretor/builtins] update builtins cause ast change

--- lib/lisp/Interpretor.hs
+++ lib/lisp/Interpretor.hs
@@ -31,28 +31,30 @@ data Env =  DefinedExpr     Identifier Expr
     deriving Show
 
 -- BUILTINS
--- applyBuiltin :: String -> [Integer] -> Maybe Expr
--- applyBuiltin "+" n = Just $ Int $ sum n
--- applyBuiltin "-" n = case n of
---     []  -> Nothing
---     [x] -> Just $ Int (-x)
---     (x:xs) -> Just $ Int $ x - sum xs
--- applyBuiltin "*" n = Just $ Int $ product n
--- applyBuiltin "div" n = case n of
---     [a,b] -> if b == 0 then Nothing else Just $ Int (a `div` b)
---     _ -> Nothing
--- applyBuiltin "mod" n = case n of
---     [a,b] -> if b == 0 then Nothing else Just $ Int (a `mod` b)
---     _ -> Nothing
--- applyBuiltin "<" n = case n of
---     [a,b] -> Just $ Boolean (a < b)
---     _ -> Nothing
--- applyBuiltin ">" n = case n of
---     [a,b] -> Just $ Boolean (a > b)
---     _ -> Nothing
--- applyBuiltin "eq?" n = case n of
---     [a,b] -> Just $ Boolean (a == b)
---     _ -> Nothing
+applyBuiltin :: Identifier -> [Integer] -> Maybe Expr
+applyBuiltin "+" n = Just $ Int $ sum n
+applyBuiltin "-" n = case n of
+    [] -> Nothing
+    [x] -> Just $ Int (-x)
+    (x:xs) -> Just $ Int (x - sum xs)
+applyBuiltin "*" n = Just $ Int $ product n
+applyBuiltin "div" n = case n of
+    [a,b] -> if b == 0 then Nothing else Just $ Int (a `div` b)
+    _ -> Nothing
+applyBuiltin "mod" n = case n of
+    [a,b] -> if b == 0 then Nothing else Just $ Int (a `mod` b)
+    _ -> Nothing
+applyBuiltin "<" n = case n of
+    [a,b] -> Just $ Boolean (a < b)
+    _ -> Nothing
+applyBuiltin ">" n = case n of
+    [a,b] -> Just $ Boolean (a > b)
+    _ -> Nothing
+applyBuiltin "eq?" n = case n of
+    [a,b] -> Just $ Boolean (a == b)
+    _ -> Nothing
+applyBuiltin _ _ = Nothing
+
 
 builtinToken :: [Identifier]
 builtinToken = [ "+", "-", "*", "div", "mod"]
