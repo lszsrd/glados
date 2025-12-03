d918b4b85caf434dc32643eab6565853484cabb4
nate <nathan.flachat@epitech.eu>
2025-12-03 15:42:11 +0100

    < 🏗️ >[Interpretor/Eval] first utilisation builtin, evaluation define ...

--- lib/lisp/Interpretor.hs
+++ lib/lisp/Interpretor.hs
@@ -1,8 +1,7 @@
-{- 
--- EPITECH PROJECT, 2025
--- GENERIC LANGUAGE AND DATA OPERAND SYNTAX
--- File description:
--- src/Parser.hs
+{- EPITECH PROJECT, 2025
+   GENERIC LANGUAGE AND DATA OPERAND SYNTAX
+   File description:
+   src/Interpretor.hs
 -}
 
 module Interpretor () where
@@ -63,41 +62,67 @@ builtinComparisonToken :: [Identifier]
 builtinComparisonToken = [ ">", "<", "eq?"]
 
 checkCallToken :: Expr -> [Identifier] -> Maybe Identifier
-checkCallToken fcall@(Call (Var id) _) (x:xs)
+checkCallToken (Call (Var id) _) (x:xs)
     | id == x = Just id
-    | otherwise = checkCallToken fcall xs
+    | otherwise = checkCallToken (Call (Var id) []) xs
 checkCallToken _ [] = Nothing
+checkCallToken _ _  = Nothing
 
-searchInEnv :: [Env] -> Expr -> Maybe Expr
-searchInEnv [] _ = Nothing
-searchInEnv ((DefinedExpr i1 _):xs) fCall@(Call (Var i2) _) =
-    if i1 == i2
-        then Just fCall
-        else searchInEnv xs fCall
-    -- TODO Need to verify types of args
-searchInEnv (_:xs) f = searchInEnv xs f
+lookupDefined :: [Env] -> Identifier -> Maybe Expr
+lookupDefined [] _ = Nothing
+lookupDefined (DefinedExpr name body : xs) key
+    | name == key = Just body
+    | otherwise   = lookupDefined xs key
+
+evalArgToInt :: Expr -> [Env] -> Maybe Integer
+evalArgToInt (Int n) _ = Just n
+evalArgToInt (Var name) env = case lookupDefined env name of
+    Just (Int n) -> Just n
+    _ -> Nothing
+evalArgToInt call@(Call (Var fname) args) env = do
+    b <- checkCallToken call (builtinToken ++ builtinComparisonToken)
+    ints <- mapM (`evalArgToInt` env) args
+    resExpr <- applyBuiltin b ints
+    case resExpr of
+        Int n -> Just n
+        _     -> Nothing
+evalArgToInt _ _ = Nothing
+
+
+evalArgs :: [Expr] -> [Env] -> Maybe [Expr]
+evalArgs [] _ = Just []
+evalArgs (a:as) env = do
+    av <- eval a env
+    rest <- evalArgs as env
+    return (av:rest)
 
 eval :: Expr -> [Env] -> Maybe Expr
-eval call@(Call (Lambda id bdy) ag) globEnv = Nothing
-eval call@(Call (Var id) ag) globEnv = 
-    case searchInEnv globEnv call of
-        Nothing -> do
-            builtin <- checkCallToken call builtinToken
-            -- applyBuiltin builtin ag
-            Nothing
-        Just f -> Nothing
-eval _ _ = Nothing
 
--- Ram through the AST and interpret it  
+eval e@(Int _) _ = Just e
+eval e@(Boolean _) _ = Just e
+eval l@(Lambda _ _) _ = Just l
+
+-- eval (If cond th el) env = do
+--     c <- eval cond env
+--     case c of
+--         Boolean True  -> eval th env
+--         Boolean False -> eval el env
+--         _             -> Nothing
+
+
 interpret :: [Ast] -> [Env] -> Maybe Ast
-
+interpret [] _ = Nothing
 interpret (Define ex body: ast) env = interpret ast (DefinedExpr ex body: env)
-interpret ((Expression f1@(Call id args)): ast) env = do
-    expr <- eval f1 env
-    interpret ast env
-interpret (Expression (Lambda args bdy): ast) env = Nothing -- Scan env to find FunctionCall and evalualte it with given args
-interpret (Expression (If cond th el): ast) env = Nothing -- Eval cond, if true, exec th else el
-interpret _ _ = Nothing
+interpret ((Expression f1@(Call _ _)): ast) env = do
+    res <- eval f1 env
+    return (Expression res)
+interpret ((Expression expr): ast) env = do
+    res <- eval expr env
+    return (Expression res)
+
+
+
+
 
 
 -- checkArgs :: Args -> Args -> Args -> Maybe Args
@@ -121,3 +146,14 @@ interpret _ _ = Nothing
 --         Left err -> printError (show err)
 --         Right str -> return str
 -- [(Define "foo" ["a", "b"] (Call "+" ["a", "b"]))]
+
+
+-- searchInEnv :: [Env] -> Expr -> Maybe Expr
+-- searchInEnv [] _ = Nothing
+-- searchInEnv ((DefinedExpr i1 _):xs) fCall@(Call (Var i2) _) =
+--     if i1 == i2
+--         then Just fCall
+--         else searchInEnv xs fCall
+-- searchInEnv (_:xs) f = searchInEnv xs f
+
+-- TESTS
