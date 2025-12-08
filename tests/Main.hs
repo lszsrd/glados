a9905ab28f3aeae5a489237aa5a7de1797a069b1
h <hugo.duda@protonmail.com>
2025-12-08 12:34:06 +0100

    < :white_check_mark: >[Testing/TestingParser] Add some parser tests

--- tests/Main.hs
+++ tests/Main.hs
@@ -11,9 +11,11 @@ import Test.HUnit
 import System.Exit (exitSuccess, exitFailure)
 import LexerTests (lexerTestsLexemes, lexerTestsLexer, lexerTestsAnyToken, lexerTestsToken, lexerTestsIdentifier, lexerTestsUInteger, lexerTestsConstant)
 import InterpretorTests (interpretorTestsSimpleSubjet, interpretorExtended, interpretorTestsBuiltins, interpretorTestsEnv)
+import ParserTests (parserTestsSimple)
 
 main :: IO ()
 main = do
     result <- runTestTT $ TestList [lexerTestsLexemes, lexerTestsLexer, lexerTestsAnyToken, lexerTestsToken, lexerTestsIdentifier, lexerTestsConstant, lexerTestsUInteger]
     result2 <- runTestTT $ TestList [interpretorTestsSimpleSubjet, interpretorExtended, interpretorTestsBuiltins, interpretorTestsEnv]
+    result3 <- runTestTT $ TestList [parserTestsSimple]
     if failures result > 0 || failures result2 > 0 then exitFailure else exitSuccess
