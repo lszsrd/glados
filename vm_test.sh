478ce6cbf163cf770e0bd650ef189e07fcca0118
h <hugo.duda@protonmail.com>
2026-01-18 16:05:36 +0100

    < :white_check_mark: && :test_tube: >[dev/FunctionnalTests] Add functionnal tests for vm and compiler

--- vm_test.sh
+++ vm_test.sh
@@ -4,34 +4,20 @@ make
 if [[ $? -eq 1 ]]; then
     echo -e "\e[31mCompilation failed\e[0m"
 fi
-mapfile -t valid_test < <(find ./tests/valid_test/ -maxdepth 1 -type f \( -name "*.rz" -o -name "*.scm" \) )
-mapfile -t bad_test < <(find ./tests/bad_test/ -maxdepth 1 -type f \( -name "*.rz" -o -name "*.scm" \) )
-mapfile -t lib_rizz < <(find ./tests/lib_rizz/ -maxdepth 1 -type f \( -name "*.rz" -o -name "*.scm" \) )
+mapfile -t valid_test < <(find ./tests/valid_test/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
+mapfile -t bad_test < <(find ./tests/bad_test/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
+mapfile -t lib_rizz < <(find ./tests/lib_rizz/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
 
 echo -e "\e[1;97m-----------------------------------------------\e[0m"
 echo -e "\e[1;33m--- VALID FILES TESTS ---\e[0m"
 
 for f in "${valid_test[@]}"; do
     echo -e "\e[1;97m-----------------------------------------------\e[0m"
-    ./glados-compiler $f > /dev/null
+    ./glados-vm $f > /dev/null
     if [[ $? -eq 1 ]]; then
         echo -e "\e[31m${f##*/} KO\e[0m"
     else
         echo -e "\e[32m${f##*/} OK\e[0m"
-    fi
-done
-
-echo -e "\e[1;97m-----------------------------------------------\e[0m"
-echo -e "\n\e[1;97m-----------------------------------------------\e[0m"
-echo -e "\e[1;33m--- BAD FILES TESTS ---\e[0m"
-
-for f in "${bad_test[@]}"; do
-    echo -e "\e[1;97m-----------------------------------------------\e[0m"
-    ./glados-compiler $f 2> /dev/null
-    if [[ $? -eq 1 ]]; then
-        echo -e "\e[32m${f##*/} OK\e[0m"
-    else
-        echo -e "\e[31m${f##*/} KO\e[0m"
     fi
 done
 
@@ -41,7 +27,7 @@ echo -e "\e[1;33m--- LIB RIZZ TESTS ---\e[0m"
 
 for f in "${lib_rizz[@]}"; do
     echo -e "\e[1;97m-----------------------------------------------\e[0m"
-    ./glados-compiler $f > /dev/null
+    ./glados-vm $f > /dev/null
     if [[ $? -eq 1 ]]; then
         echo -e "\e[31m${f##*/} KO\e[0m"
     else
