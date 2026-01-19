c11f3842295be777e6d421c86a45b889c6a5d08e
lszsrd <laszlo.serdet@epitech.eu>
2026-01-19 10:55:46 +0100

    < :memo: >[glados] Updated tests

--- tests/vm_test.sh
+++ tests/vm_test.sh
@@ -4,16 +4,16 @@ make
 if [[ $? -eq 1 ]]; then
     echo -e "\e[31mCompilation failed\e[0m"
 fi
-mapfile -t valid_test < <(find ./tests/valid_test/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
-mapfile -t bad_test < <(find ./tests/bad_test/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
-mapfile -t lib_rizz < <(find ./tests/lib_rizz/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
+mapfile -t valid_test < <(find ./valid_test/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
+mapfile -t bad_test < <(find ./bad_test/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
+mapfile -t lib_rizz < <(find ./lib_rizz/ -maxdepth 1 -type f \( -name "*.rz.bc" -o -name "*.scm.bc" \) )
 
 echo -e "\e[1;97m-----------------------------------------------\e[0m"
 echo -e "\e[1;33m--- VALID FILES TESTS ---\e[0m"
 
 for f in "${valid_test[@]}"; do
     echo -e "\e[1;97m-----------------------------------------------\e[0m"
-    ./glados-vm $f > /dev/null
+    ../glados-vm $f > /dev/null
     if [[ $? -eq 1 ]]; then
         echo -e "\e[31m${f##*/} KO\e[0m"
     else
@@ -27,7 +27,7 @@ echo -e "\e[1;33m--- LIB RIZZ TESTS ---\e[0m"
 
 for f in "${lib_rizz[@]}"; do
     echo -e "\e[1;97m-----------------------------------------------\e[0m"
-    ./glados-vm $f > /dev/null
+    ../glados-vm $f > /dev/null
     if [[ $? -eq 1 ]]; then
         echo -e "\e[31m${f##*/} KO\e[0m"
     else
